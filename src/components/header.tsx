7e933e8fc2226fbbc762e3df2b00e15bca448d7e
Brian Faubion <powerinblack@gmail.com>
2025-08-25 22:41:55 -0400

    Added alias display to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,61 +1,57 @@
-import { options, SessionUser } from "@/lib/auth";
-import { getServerSession } from "next-auth";
+import { SessionUser } from "@/lib/auth";
 import Image from "next/image";
 import { ReactElement } from "react";
 import { NavigationButton, LinkButton } from "./navigation";
 import { Alias } from "@prisma/client";
 
 /** Displays a user's current status and gives them the option to sign in or out */
-function UserComponent(): ReactElement
+function UserComponent(props: {user: SessionUser | null, alias: Alias | null}): ReactElement
 {
+	let inner_components = <LinkButton label="Sign In" target="/api/auth/signin" />;
+	
+	if (props.user) {
+		if (props.alias) {
+			// Display the user's current alias
+			const img = props.alias?.image;
+			inner_components = <>
+				<div key={1}>{img && <Image src={img} alt="Icon" width={32} height={32} />}</div>
+				<div>{props.alias.name}</div>
+				<LinkButton label="Sign Out" target="/api/auth/signout" />
+			</>
+		} else {
+			// Display a button to create an account if the user doesn't have an alias yet
+			inner_components = <>
+				<LinkButton label="Set Up Account" target="/signup" />
+				<LinkButton label="Sign Out" target="/api/auth/signout" />
+			</>
+		}
+	}
+
 	return (
-		<LinkButton label="Sign In" target="/api/auth/signin" key={1} />
+		<div className="flex flex-row items-center gap-2">
+			{inner_components}
+		</div>
 	);
 }
 
 /** A set of navigation buttons that provide different options depending on the user's status */
-function NavigationBar(): ReactElement
+function NavigationBar(props: {alias: Alias | null}): ReactElement
 {
 	return (
-		<div>
+		<div className="flex flex-row gap-2 font-bold flex-1">
 			<NavigationButton label="Home" target="/" />
-			<NavigationButton label="Posts" target="/history" />
-			<NavigationButton label="Create" target="/create" />
+			{props.alias && <NavigationButton label="Posts" target="/history" />}
+			{props.alias && <NavigationButton label="Create" target="/create" />}
 		</div>
 	)
 }
 
 export async function Header(props: {user: SessionUser | null, alias: Alias | null}): Promise<ReactElement>
 {
-	let left_components = [<NavigationButton label="Home" target="/" key={1} />];
-	let right_components = [<LinkButton label="Sign In" target="/api/auth/signin" key={1} />];
-
-	// Switch components if the user is signed in
-	if (props.user) {
-		// Get user data from the session
-		const user = props.user.name;
-		const img = props.user?.image;
-
-		right_components = [
-			<div key={1}>{img && <Image src={img} alt="Icon" width={32} height={32} />}</div>,
-			<div key={2}>{user}</div>,
-			<LinkButton label="Create Account" target="/signup" key={3} />,
-			<LinkButton label="Sign Out" target="/api/auth/signout" key={4} />
-		];
-		left_components = [
-			<NavigationButton label="Home" target="/" key={1} />,
-			<NavigationButton label="Posts" target="/history" key={2} />,
-			<NavigationButton label="Create" target="/create" key={3} />,
-		];
-	}
 	return (
 		<div className="flex flex-row items-center gap-2 p-2 w-full sticky top-0 bg-slate-400">
-			<div className="flex flex-row gap-2 font-bold flex-1">
-				{left_components}
-			</div>
-			<div className="flex flex-row items-center gap-2">
-				{right_components}
-			</div>
+			<NavigationBar alias={props.alias} />
+			<UserComponent user={props.user} alias={props.alias} />
 		</div>
 	);
 }
